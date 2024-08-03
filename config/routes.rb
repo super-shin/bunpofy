25e6a411e2b108148ff8164500e9c3c2271f3538
Junior Penn <pennchojunior@gmail.com>
2024-08-03 18:08:33 +0900

    Completed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,21 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :teacher do
+    resources :submissions, only: [:index] do
+      resources :feedbacks, only: [:new, :create]
+    end
     resources :challenges
   end
 
   namespace :student do
-    resources :challenges, only: [:index]
+    resources :challenges, only: [:index, :show] do
+      resources :submissions, only: [:new, :create]
+    end
+    resources :submissions, only: [:show] do
+      resources :games, only: [:show, :edit, :update]
+    end
   end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
