1e0fe825c123057707d7397e42b77a2795c11f55
Pedro Kogawa <153990775+pedrokogawa@users.noreply.github.com>
2024-08-04 11:43:01 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :classrooms
   has_many :feedbacks
   has_many :submissions
+  has_many :challenges
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
