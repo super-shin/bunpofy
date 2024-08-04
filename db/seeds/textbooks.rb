077c1bbd45ea9bc96ce794bff96d23ef8b86632a
Shingie Taira <tairashingie@gmail.com>
2024-08-04 09:26:36 +0900

    fixed headers and added notifications during seed

--- db/seeds/textbooks.rb
+++ db/seeds/textbooks.rb
@@ -16,22 +16,33 @@ puts "Destroyed Words"
 Textbook.destroy_all
 puts "Destroyed Textbooks"
 
+# Reset ID key sequences
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE word_references, units, words, textbooks RESTART IDENTITY CASCADE")
+
 #iterate over the file path array for multiple textbooks
+total_rows = 2193
+current_index = 0
 puts "Entering seeds..."
 file_paths.each do |file_path|
-  CSV.foreach(file_path, headers: true) do |row|
+  CSV.foreach(file_path, encoding: 'UTF-8', headers: true, header_converters: :symbol) do |row|
     #find or create the word
-    word = Word.find_or_create_by!(english: row['english'], japanese: row['japanese'], phrase: row['phrase'], level: row['level'], grade: row['grade'].to_i)
+    word = Word.find_or_create_by!(english: row[:english], japanese: row[:japanese], phrase: row[:phrase], level: row[:level], grade: row[:grade].to_i)
     #find or create the textbook
-    textbook = Textbook.find_or_create_by!(name: row['textbook'])
+    textbook = Textbook.find_or_create_by!(name: row[:textbook])
     #find or create the unit with the textbook
-    unit_base = row['unit']
-    part_name = row['part']
+    unit_base = row[:unit]
+    part_name = row[:part]
     unit_name = part_name.nil? ? unit_base.strip : "#{unit_base.strip} - #{part_name.strip}"
     unit = Unit.find_or_create_by!(name: unit_name, textbook: textbook)
     #create wordReference
-    word_reference = WordReference.find_or_create_by!(unit: unit, word: word, page: row['page'].to_i)
-    puts "#{word.english} created"
+    word_reference = WordReference.find_or_create_by!(unit: unit, word: word, page: row[:page].to_i)
+    
+    # Update progress
+    current_index = current_index + 1
+    percentage = (current_index.to_f / total_rows * 100).round(2)
+    progress = (current_index.to_f / total_rows * 100).to_i
+    progress_display = "PROGRESS:" + "/" * progress
+    print "\r#{progress_display} #{percentage}%"
   end
-  puts "End of Seeds"
+  puts "\nSeeding ALL DONE!"
 end
