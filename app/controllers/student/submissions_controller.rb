551d2395fae8c4416e3103166842331cdf1bf940
Shingie Taira <tairashingie@gmail.com>
2024-08-07 17:37:33 +0900

    coded entire sentensensei stimulus

--- app/controllers/student/submissions_controller.rb
+++ app/controllers/student/submissions_controller.rb
@@ -1,20 +1,25 @@
 class Student::SubmissionsController < ApplicationController
-  before_action :set_submission, only: [:show, :create]
+  before_action :set_submission, only: [:show]
+  before_action :set_challenge, only: [:new, :create]
 
   def show
     # @submission it's already set by before_action
   end
 
   def new
-    @submission = Submission.new
+    @submission = @challenge.submissions.build
   end
 
   def create
-    @submission = Submission.new(submission_params)
+    @submission = @challenge.submissions.build(submission_params)
+    @submission.user = current_user
+    ai_response_data = JSON.parse(@submission.ai_response)
+    @submission.score = ai_response_data['info']['score'].to_i
     if @submission.save
       redirect_to student_submission_path(@submission), notice: 'Submission was successfully created.'
     else
       render :new
+      Rails.logger.error("Submission save failed: #{@submission.errors.full_messages.join(', ')}")
     end
   end
 
@@ -22,6 +27,10 @@ class Student::SubmissionsController < ApplicationController
 
   def set_submission
     @submission = Submission.find(params[:id])
+  end
+
+  def set_challenge
+    @challenge = Challenge.find(params[:challenge_id])
   end
 
   def submission_params
