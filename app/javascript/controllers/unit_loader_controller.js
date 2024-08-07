084e87ebcebeec427e175f3e260988b9665161d9
Shingie Taira <tairashingie@gmail.com>
2024-08-07 12:52:34 +0900

    added units loader stimulus controller for autoloading units

---
+++ app/javascript/controllers/unit_loader_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="unit-loader"
+export default class extends Controller {
+  static targets = ['textbookSelect', 'unitSelect']
+
+  connect() {
+    this.unitsByTextbook = JSON.parse(this.element.dataset.unitsByTextbook)
+  }
+
+  updateUnits() {
+    const textbookId = this.textbookSelectTarget.value
+
+    this.unitSelectTarget.innerHTML = '<option value ="">Select a unit</option>'
+
+    if (textbookId && this.unitsByTextbook[textbookId]) {
+      this.unitsByTextbook[textbookId].forEach(unit => {
+        const option = document.createElement('option')
+        option.value = unit.id
+        option.textContent = unit.name
+        this.unitSelectTarget.appendChild(option)
+      })
+    }
+  }
+}
