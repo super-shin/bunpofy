0d2da325d7c4e6621a270b4fdfe2fee559ea6217
Rei Zhang <mauoser@outlook.com>
2024-08-10 16:14:51 +0900

    fixed challenges index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_10_030328) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_08_083714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_030328) do
   end
 
   create_table "feedbacks", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
