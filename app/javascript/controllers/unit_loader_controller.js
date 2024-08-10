7444b909d7f824276ca988eb06ae2e3786557505
Shingie Taira <tairashingie@gmail.com>
2024-08-10 10:27:38 +0900

    changed pictures

--- app/javascript/controllers/unit_loader_controller.js
+++ app/javascript/controllers/unit_loader_controller.js
@@ -24,37 +24,37 @@ export default class extends Controller {
 
     // Define a mapping of textbookId to image URLs
     const imageUrls = {
-      1: 'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203907/1_wvofee.png',
-      2: 'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203908/2_q1gpvx.png',
-      3:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203906/3_lgjwvp.png',
-      4:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203906/4_ottud0.png',
-      5:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203905/5_etw3uh.png',
-      6:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203904/6_w7eyma.png',
-      7:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203905/8_qc6y79.png',
-      8:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203905/7_ieb5xo.png',
-      9:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203905/9_radpyt.png',
+      1: 'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252490/19_v74swk.png',
+      2: 'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252491/18_mpqsvl.png',
+      3:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252491/17_h8a5ja.png',
+      4:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252506/16_ml8xpy.png',
+      5:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252506/15_mxkpop.png',
+      6:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252506/14_uboyzd.png',
+      7:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252507/12_omkjxl.png',
+      8:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252507/13_cooclj.png',
+      9:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252507/11_ekromu.png',
       10:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203905/10_a7eq5l.png',
       11:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203882/11_ds5y3z.png',
-      12:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203861/12_ki2xcv.png',
-      13:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203848/13_piipat.png',
-      14:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203849/17_v3dimr.png',
-      15:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203845/18_vs7s32.png',
-      16:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203851/19_j6qebm.png',
-      17:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203845/20_tejg2m.png',
-      18:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203846/21_tmq6s5.png',
-      19:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203841/26_jshtna.png',
+      12:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252513/8_xlkxxk.png',
+      13:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252513/7_i12474.png',
+      14:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252514/3_z9ofxc.png',
+      15:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252518/2_cdldnz.png',
+      16:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252519/1_kqgugr.png',
+      17:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252488/30_mlazjs.png',
+      18:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252489/29_wmhzmi.png',
+      19:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252490/25_sub7lm.png',
       20:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203842/25_vgnwlq.png',
-      21:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203845/22_a4ijvu.png',
-      22:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203846/23_je1r7p.png',
-      23:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203843/24_lftluo.png',
-      24:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203844/27_v1zizt.png',
-      25:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203843/30_tb4sp1.png',
-      26:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203843/28_wd4sex.png',
-      27:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203842/31_fjix53.png',
-      28:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203843/29_hzacbj.png',
-      29:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203849/14_isgthv.png',
-      30:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203848/15_hn4okv.png',
-      31:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723203847/16_udmklw.png'
+      21:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252489/28_cfxkyt.png',
+      22:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252489/26_kdt0mg.png',
+      23:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252489/27_mzkn4f.png',
+      24:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252490/23_qqujgc.png',
+      25:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252490/21_oh1od5.png',
+      26:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252490/22_wlzak2.png',
+      27:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252491/20_nxvegp.png',
+      28:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252520/Untitled_design_xygzy8.png',
+      29:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252513/6_zxn5fz.png',
+      30:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252513/5_poft3h.png',
+      31:'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723252513/4_waq1hl.png'
       // Add more mappings as needed
     };
 
@@ -66,7 +66,7 @@ export default class extends Controller {
       imgElement.src = imageUrls[textbookId];
     } else {
       // Optionally handle the case where no URL is found for the given textbookId
-      imgElement.src = 'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723206796/www.bunpofy.me_po61ps.png'; // Clear the image source or set a default image
+      imgElement.src = 'https://res.cloudinary.com/ddzvfukq6/image/upload/v1723251348/Untitled_design_oswzkh.gif'; // Clear the image source or set a default image
     }
   }
 }
