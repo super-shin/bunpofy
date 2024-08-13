41023dcd39e34ba209e860500c7df693c36df815
Pedro Kogawa <pedrokogawa@gmail.com>
2024-08-13 18:14:42 +0900

    student-challenges page implementation

--- app/controllers/student/challenges_controller.rb
+++ app/controllers/student/challenges_controller.rb
@@ -3,6 +3,10 @@ class Student::ChallengesController < ApplicationController
 
   def index
     @challenges = policy_scope(Challenge)
+    # Defining array for leaderboard users
+    @classrooms = current_user.classrooms_as_student
+    @students = @classrooms.flat_map(&:students)
+    ## Implement the level system here whem it is done
   end
 
   def show
