3b59b8b9fb083fe6f415d763d2005819cb6c5635
Junior Penn <pennchojunior@gmail.com>
2024-08-13 22:15:36 +0900

    spelling game

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # SEEDS for BUNPOFY
 # RULE: Don't change the Order of anything unless checked thoroughly
 
-#STEP 1: Destroy ALL
-##Destroy all Games
+# STEP 1: Destroy ALL
+#Destroy all Games
 Game.destroy_all
 puts "Destroyed Games"
 #Attendance.destroy_all
@@ -17,29 +17,29 @@ puts "Destroyed Submissions"
 # #Destroy all Challenges
 Challenge.destroy_all
 puts "Destroyed Challenges"
-# #Destroy all Classrooms
+#Destroy all Classrooms
 Classroom.destroy_all
 puts "Destroyed Classrooms"
-# #Destroy all WordReferences
-WordReference.destroy_all
-puts "Destroyed Word References"
-# #Destroy all Units
-Unit.destroy_all
-puts "Destroyed Units"
-# #Destroy all Words
-Word.destroy_all
-puts "Destroyed Words"
-# #Destroy all Textbooks  ###### 
+#Destroy all WordReferences
+# WordReference.destroy_all
+# puts "Destroyed Word References"
+# # #Destroy all Units
+# Unit.destroy_all
+# puts "Destroyed Units"
+# # #Destroy all Words
+# Word.destroy_all
+# puts "Destroyed Words"
+# #Destroy all Textbooks  ######
 # Textbook.destroy_all
 # puts "Destroyed Textbooks" ######
 # #Destroy all Users
 User.destroy_all
 puts "Destroyed Users"
-# #Reset ID key sequences
+#Reset ID key sequences
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE units, words, textbooks, users, classrooms, attendances RESTART IDENTITY CASCADE")
 puts "RESET ID Numbers"
 
-#STEP 2: SEED Textbooks --- ######
+# STEP 2: SEED Textbooks --- ######
 # require_relative 'seeds/textbooks'
 
 
