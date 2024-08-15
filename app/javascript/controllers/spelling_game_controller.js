0000ccd889b6f762c70b65f42d214d321c1becfa
Junior Penn <pennchojunior@gmail.com>
2024-08-15 21:55:51 +0900

    Getting words from Textbook

--- app/javascript/controllers/spelling_game_controller.js
+++ app/javascript/controllers/spelling_game_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="spelling-game"
 export default class extends Controller {
+  static values = { wordsArray: Array }
   connect() {
+    const wordsArray = (this.wordsArrayValue);
+    console.log("Selected words for the game:", wordsArray);
+  }
+
+  textToSpeech() {
+    const utterance = new SpeechSynthesisUtterance(wordsArray);
+    window.speechSynthesis.speak(utterance);
   }
 }
