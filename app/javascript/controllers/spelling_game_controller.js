3178f8aa9d758ef16df38ef675a2f25a8f3eb878
Junior Penn <pennchojunior@gmail.com>
2024-08-16 13:43:56 +0900

    Made the condition correct and incorrect  words

--- app/javascript/controllers/spelling_game_controller.js
+++ app/javascript/controllers/spelling_game_controller.js
@@ -1,16 +1,41 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="spelling-game"
 export default class extends Controller {
-  static values = { wordsArray: Array }
+  static values = { wordsArray: Array };
+  static targets = ['input', 'results'];
+
   connect() {
-    const wordsArray = (this.wordsArrayValue);
-    console.log("Selected words for the game:", wordsArray);
+    this.generateRandomWord();
+  }
+
+  generateRandomWord() {
+    const randomIndex = Math.floor(Math.random() * this.wordsArrayValue.length);
+    this.randomWord = this.wordsArrayValue[randomIndex];
+    // console.log('Selected word for the game:', this.randomWord);
+  }
+
+  correctSpelling(event) {
+    event.preventDefault();
+    const resultElement = this.resultsTarget
+    const userInput = this.inputTarget.value.trim().toLowerCase();
+    if (userInput === this.randomWord.toLowerCase()) {
+      resultElement.innerHTML =
+      `<p id="correct">
+          Correct, You did great!
+          <i class="fa fa-check-circle" aria-hidden="true"></i>
+        </p>`;
+    } else {
+      resultElement.innerHTML = `
+      <p id="incorrect">
+        Incorrect, Please try again!
+        <i class="fa fa-times-circle" aria-hidden="true"></i>
+      </p>`;
+    }
   }
 
   textToSpeech() {
-    const speechText = this.wordsArrayValue.join(" ");
-    const utterance = new SpeechSynthesisUtterance(speechText);
+    const utterance = new SpeechSynthesisUtterance(this.randomWord);
     window.speechSynthesis.speak(utterance);
   }
 }
