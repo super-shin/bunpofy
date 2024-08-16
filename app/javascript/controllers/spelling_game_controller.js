fed4c1e08ef4e53c9a27b4aeaa0abea7c9215cff
Junior Penn <pennchojunior@gmail.com>
2024-08-16 16:12:46 +0900

    Margins and Sound implimented

--- app/javascript/controllers/spelling_game_controller.js
+++ app/javascript/controllers/spelling_game_controller.js
@@ -2,11 +2,13 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="spelling-game"
 export default class extends Controller {
-  static values = { wordsArray: Array };
+  static values = { wordsArray: Array, correctSound: String, incorrectSound: String };
   static targets = ['input', 'results'];
 
   connect() {
     this.generateRandomWord();
+    this.correctSound = new Audio (this.correctSoundValue);
+    this.incorrectSound = new Audio (this.incorrectSoundValue)
   }
 
   generateRandomWord() {
@@ -17,20 +19,41 @@ export default class extends Controller {
 
   correctSpelling(event) {
     event.preventDefault();
-    const resultElement = this.resultsTarget
+    const resultElement = this.resultsTarget;
     const userInput = this.inputTarget.value.trim().toLowerCase();
     if (userInput === this.randomWord.toLowerCase()) {
-      resultElement.innerHTML =
-      `<p id="correct">
-          Correct, You did great!
-          <i class="fa fa-check-circle" aria-hidden="true"></i>
-        </p>`;
-    } else {
+      this.correctSound.play();
       resultElement.innerHTML = `
-      <p id="incorrect">
-        Incorrect, Please try again!
+
+        <div class="d-flex align-items-center justify-content-center">
+          <i class="fa fa-check-circle" aria-hidden="true"></i>
+          <p id="correct">
+          Correct, You did great!</p>
+        </div>
+        <i class="fa fa-forward fa-2x text-white" id="next-word-button"></i>
+        <div class="firework"></div>
+        <div class="firework"></div>
+      `;
+      document.getElementById('next-word-button').addEventListener('click', () => {
+        this.inputTarget.value = '';
+        this.generateRandomWord();
+        resultElement.innerHTML = '';
+      });
+    } else {
+      this.incorrectSound.play();
+      resultElement.innerHTML = `
+      <div class="d-flex align-items-center justify-content-center">
         <i class="fa fa-times-circle" aria-hidden="true"></i>
-      </p>`;
+        <p id="incorrect">
+          Incorrect, Please try again!
+        </p>
+      </div>
+        <i class="fa fa-rotate-right fa-2x text-white" id="try-again-button"></i>
+      `;
+      document.getElementById('try-again-button').addEventListener('click', () => {
+        this.inputTarget.value = '';
+        resultElement.innerHTML = '';
+      });
     }
   }
 
