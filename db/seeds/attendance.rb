985a3b15373ad0fa5b75dd2ef4e4ed4376008715
Shingie Taira <tairashingie@gmail.com>
2024-08-21 21:12:01 +0900

    adjuster attendance classrooms and challenges seeds

--- db/seeds/attendance.rb
+++ db/seeds/attendance.rb
@@ -1,5 +1,5 @@
 puts "Seeding Attendance..."
-#Seeding 15 Students in 1 Class
+#Seeding Students in 2 Classes each
 puts "Seeding Class A to Classroom 1 + 4"
 (5..24).each do |t|
   Attendance.create([{classroom_id: 1, user_id: t}, {classroom_id: 4, user_id: t}])
@@ -11,12 +11,12 @@ puts "Seeding Class B to Classroom 2 + 5"
   puts "Enrolled #{User.find(t).first_name} to #{Classroom.find(2).name} and #{Classroom.find(5).name}"
 end
 puts "Seeding Class C to Classroom 3 + 6"
-(44..64).each do |t|
+(44..58).each do |t|
   Attendance.create([{classroom_id: 3, user_id: t}, {classroom_id: 6, user_id: t}])
   puts "Enrolled #{User.find(t).first_name} to #{Classroom.find(3).name} and #{Classroom.find(6).name}"
 end
 puts "Seeding Class D to Classroom 7"
-(65..79).each do |t|
+(59..79).each do |t|
   Attendance.create([{classroom_id: 7, user_id: t}])
   puts "Enrolled #{User.find(t).first_name} to #{Classroom.find(7).name}"
 end
